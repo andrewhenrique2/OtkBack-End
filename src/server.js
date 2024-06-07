5530706a2c17a87002946e93aadca64f6fd75aee
andrewhenrique2 <andrewloly06@gmail.com>
2024-06-07 03:07:09 -0300

    tab

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
+const helmet = require('helmet');
 const animeRoutes = require('./routes/animeRoutes');
 const filmRoutes = require('./routes/filmRoutes');
 const authRoutes = require('./routes/authRoutes');
@@ -10,20 +10,39 @@ const recentAnimeRoutes = require('./routes/recentAnimeRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Configurar CSP
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    imgSrc: ["'self'", 'data:', 'https:'],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    styleSrc: ["'self'", "'unsafe-inline'"],
+    connectSrc: ["'self'", 'https://kitsu.io']
+  }
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// Adicionando rotas
 app.use('/api', animeRoutes);
 app.use('/api', filmRoutes);
 app.use('/api', recentAnimeRoutes);
 app.use('/auth', authRoutes);
 
+// Adicionando rota básica para verificar a API
+app.get('/', (req, res) => {
+  res.send('API está funcionando');
+});
+
+// Middleware para lidar com erros
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Algo deu errado!');
 });
 
+// Iniciando o servidor
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
